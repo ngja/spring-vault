bed4ee52ac7e5a9a3c2b563cf0ba1faf0dc2ecdb
hansol <wkdqhrh00@naver.com>
2022-09-26 23:36:46 +0900

    UserDetails, UserDetailsService 구현

--- vault-security/src/main/java/com/hansol/springvault/config/ProjectConfig.java
+++ vault-security/src/main/java/com/hansol/springvault/config/ProjectConfig.java
@@ -1,27 +1,24 @@
 package com.hansol.springvault.config;
 
+import com.hansol.springvault.service.InMemoryUserDetailsService;
+import com.hansol.springvault.user.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
 
 @Configuration
-public class UserManagementConfig {
+public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-        return userDetailsService;
+        UserDetails u = new User("john", "12345", "read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
