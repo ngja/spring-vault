b1936881be0f6bc1b8a1ea031b18e3ac85ad6242
hansol <wkdqhrh00@naver.com>
2022-09-28 22:06:06 +0900

    Jdbc를 이용한 UserDetailsService

--- vault-security/build.gradle.kts
+++ vault-security/build.gradle.kts
@@ -3,5 +3,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.security:spring-security-test")
 }
--- vault-security/src/main/java/com/hansol/springvault/config/ProjectConfig.java
+++ vault-security/src/main/java/com/hansol/springvault/config/ProjectConfig.java
@@ -1,24 +1,26 @@
 package com.hansol.springvault.config;
 
-import com.hansol.springvault.service.InMemoryUserDetailsService;
-import com.hansol.springvault.user.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("john", "12345", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
+        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
